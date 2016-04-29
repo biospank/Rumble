983e590a9d13c023542d6256dd3ad022ee9ad46f
fabio.petrucci <fabio.petrucci@onthebeach.co.uk>
2016-04-29 16:02:18 +0100

    Enable Rumble.User ecto model, enable Rumble.Repo worker and create a migration

--- lib/rumble/repo.ex
+++ lib/rumble/repo.ex
@@ -1,28 +1,3 @@
 defmodule Rumble.Repo do
-  #use Ecto.Repo, otp_app: :rumble
-
-  @moduledoc """
-    In memory repository
-  """
-
-  def all(Rumble.User) do
-    [
-      %Rumble.User{id: "1", name: "Josè", username: "josevalim", password: "elixir"},
-      %Rumble.User{id: "2", name: "Bruce", username: "redrapids", password: "7langs"},
-      %Rumble.User{id: "3", name: "Chris", username: "chrismccord", password: "phx"}
-    ]
-  end
-  def all(_module), do: []
-
-  def get(module, id) do
-    all(module) |> Enum.find(fn map -> map.id == id end)
-  end
-
-  def get_by(module, params) do
-    Enum.find(all(module), fn(user) ->
-      Enum.all?(params, fn({key, value}) ->
-        Map.fetch(user, key) == {:ok, value}
-      end)
-    end)
-  end
+  use Ecto.Repo, otp_app: :rumble
 end
